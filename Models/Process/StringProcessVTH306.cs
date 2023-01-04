fcaf58cdb89501084050fe1468d4d33eb88ab828
Huyen11092001 <88873971+Huyen11092001@users.noreply.github.com>
2023-01-04 08:53:38 +0700

    Create_Class_StrPro_VTH306

---
+++ Models/Process/StringProcessVTH306.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace VuThiHuyenBTH.Models.Process
+{
+    public class StringProcessVTH306
+    {
+       
+        public string LowerToUpper(string strInput)
+        {
+            string strResult = "";
+            strInput = strInput.ToUpper();
+            strResult = strInput;
+        
+            return strResult;
+        }
+        
+        
+
+
+        }
+    }
+
